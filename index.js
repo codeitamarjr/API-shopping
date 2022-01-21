193a5b1a213be67b007dfefd0f22254455d35fa5
Itamar Junior <itjunior@gmail.com>
2022-01-21 23:26:57 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const PORT = 8080
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+const itemsList = []
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Shopping API, you can go to the address shopping?item=banana to test')
+})
+
+app.get('/shopping', function(req, res) {
+    var item = req.query.item
+        //res.json("The requested item is: " + item)
+    axios.get('https://www.tesco.ie/groceries/product/search/default.aspx?searchBox=' + item)
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+
+
+            $('h3.inBasketInfoContainer', html).each(function() {
+                const product = $(this).text()
+                const price = $('span.linePrice').text()
+                    //const price = $('h3.inBasketInfoContainer').find('span.linePrice')
+                itemsList.push({
+                    product,
+                    price
+                })
+            })
+
+
+
+
+            // $('li:contains(' + item + ')', html).each(function() {
+            //     const product = $("h3.inBasketInfoContainer").text()
+            //     const price = $("span.linePrice").text()
+            //     itemsList.push({
+            //         product,
+            //         price
+            //     })
+            // })
+
+
+
+            res.json(itemsList)
+            itemsList = []
+        }).catch((err) => console.log(err))
+})
+
+app.listen(PORT, () => console.log('server runing on PORT ' + PORT))

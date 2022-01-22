d434904ef48a8b3a80ba9437aeff20f48c958a95
Itamar Junior <itjunior@gmail.com>
2022-01-22 21:24:35 +0000

    added array with shopping list

--- index.js
+++ index.js
@@ -2,62 +2,67 @@ const PORT = 8080
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { response } = require('express')
 
 const app = express()
 const itemsList = []
+
+const sourceShoppings = [{
+        name: 'Tesco',
+        address: 'https://www.tesco.ie/groceries/product/search/default.aspx?searchBox=',
+        TAGmainBlockItem: 'ul>li ',
+        TAGmainBlockItemName: 'h3.inBasketInfoContainer',
+        TAGmainBlockItemPrice: 'span.linePrice',
+        GETmainBlockItemURL: 'h3.inBasketInfoContainer>a',
+        baseForItemURL: 'https://www.tesco.ie'
+    },
+    // {
+    //     name: 'Dunnes Stores',
+    //     address: 'https://www.dunnesstoresgrocery.com/sm/delivery/rsid/258/results?q=',
+    //     TAGmainBlockItem: '',
+    //     TAGmainBlockItemName: '',
+    //     TAGmainBlockItemPrice: '',
+    //     GETmainBlockItemURL: '',
+    //     baseForItemURL: ''
+    // }
+]
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Shopping API, you can go to the address shopping?item=banana to test')
 })
 
+
+
 app.get('/shopping', function(req, res) {
     var item = req.query.item
-        //res.json("The requested item is: " + item)
-    axios.get('https://www.tesco.ie/groceries/product/search/default.aspx?searchBox=' + item)
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
 
-            $('ul>li', html).each(function() {
-                const product = $(this).find('h3.inBasketInfoContainer').text()
-                const price = $(this).find('span.linePrice').text()
-                if (!product == 0) {
-                    itemsList.push({
-                        product,
-                        price
-                    })
-                }
-            })
+    sourceShoppings.forEach(sourceShoppings => {
+        const searchAddress = sourceShoppings.address + item
+        const nameShopping = sourceShoppings.name
 
-
-
-            // $('h3.inBasketInfoContainer', html).each(function() {
-            //     const product = $(this).text()
-            //     const price = $('span.linePrice').text()
-            //         //const price = $('h3.inBasketInfoContainer').find('span.linePrice')
-            //     itemsList.push({
-            //         product,
-            //         price
-            //     })
-            // })
-
-
-
-
-            // $('li:contains(' + item + ')', html).each(function() {
-            //     const product = $("h3.inBasketInfoContainer").text()
-            //     const price = $("span.linePrice").text()
-            //     itemsList.push({
-            //         product,
-            //         price
-            //     })
-            // })
-
-
-
-            res.json(itemsList)
-            itemsList = []
-        }).catch((err) => console.log(err))
+        axios.get(searchAddress)
+            .then((response) => {
+                const html = response.data
+                const $ = cheerio.load(html)
+                $(sourceShoppings.TAGmainBlockItem, html).each(function() {
+                    const productTitle = $(this).find(sourceShoppings.TAGmainBlockItemName).text()
+                    const price = $(this).find(sourceShoppings.TAGmainBlockItemPrice).text()
+                    const url = $(this).find(sourceShoppings.GETmainBlockItemURL).attr('href')
+                    if (!productTitle == 0) {
+                        itemsList.push({
+                            nameShopping,
+                            productTitle,
+                            price,
+                            url: sourceShoppings.baseForItemURL + url
+                        })
+                    }
+                })
+                res.json(itemsList)
+            }).catch((err) => console.log(err))
+    })
 })
+
+
+
 
 app.listen(PORT, () => console.log('server runing on PORT ' + PORT))

6374ad333753d3e1ee6ff710272fc1c2b583c1f1
Itamar Junior <itjunior@gmail.com>
2022-03-22 16:59:05 +0000

    Fixed classes

--- index.js
+++ index.js
@@ -8,24 +8,23 @@ const app = express()
 const itemsList = []
 
 const sourceShoppings = [{
-        name: 'Tesco',
-        address: 'https://www.tesco.ie/groceries/en-IE/search?query=',
-        TAGmainBlockItem: '.product-tile-wrapper',
-        TAGmainBlockItemName: '.eYySMn',
-        TAGmainBlockItemPrice: '.value', //Text from class "eYySMn"
-        GETmainBlockItemURL: 'a.eYySMn', //Class eYySMn inside a link
-        baseForItemURL: 'https://www.tesco.ie'
-    },
-    // {
-    //     name: 'Dunnes Stores',
-    //     address: 'https://www.dunnesstoresgrocery.com/sm/delivery/rsid/258/results?q=',
-    //     TAGmainBlockItem: '',
-    //     TAGmainBlockItemName: '',
-    //     TAGmainBlockItemPrice: '',
-    //     GETmainBlockItemURL: '',
-    //     baseForItemURL: ''
-    // }
-]
+    name: 'Tesco Ireland',
+    address: 'https://www.tesco.ie/groceries/en-IE/search?query=',
+    TAGmainBlockItem: '.product-tile-wrapper', //The ITEM wrap class
+    TAGmainBlockItemName: '.eYySMn', //Item price from class "eYySMn"
+    TAGmainBlockItemPrice: '.value', //Text from class "eYySMn"
+    GETmainBlockItemURL: 'a.eYySMn', //Class eYySMn inside a link
+    baseForItemURL: 'https://www.tesco.ie'
+        // },
+        // {
+        //     name: 'Fresh Ireland',
+        //     address: 'https://freshonline.ie/search?type=product&q=',
+        //     TAGmainBlockItem: '.product-item--vertical',
+        //     TAGmainBlockItemName: 'a.product-item__title',
+        //     TAGmainBlockItemPrice: '.price',
+        //     GETmainBlockItemURL: 'a.product-item__image-wrapper',
+        //     baseForItemURL: 'https://freshonline.ie/'
+}]
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Shopping API, you can go to the address shopping?item=banana to test')

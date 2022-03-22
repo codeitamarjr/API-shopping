b12d5d02d899f8b0d1484d60cd0591dc3208ebe8
Itamar Junior <itjunior@gmail.com>
2022-03-22 15:20:09 +0000

    fixed from Tesco IE Website

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8088
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -9,11 +9,11 @@ const itemsList = []
 
 const sourceShoppings = [{
         name: 'Tesco',
-        address: 'https://www.tesco.ie/groceries/product/search/default.aspx?searchBox=',
-        TAGmainBlockItem: 'ul>li ',
-        TAGmainBlockItemName: 'h3.inBasketInfoContainer',
-        TAGmainBlockItemPrice: 'span.linePrice',
-        GETmainBlockItemURL: 'h3.inBasketInfoContainer>a',
+        address: 'https://www.tesco.ie/groceries/en-IE/search?query=',
+        TAGmainBlockItem: '.product-tile-wrapper',
+        TAGmainBlockItemName: '.eYySMn',
+        TAGmainBlockItemPrice: '.value', //Text from class "eYySMn"
+        GETmainBlockItemURL: 'a.eYySMn', //Class eYySMn inside a link
         baseForItemURL: 'https://www.tesco.ie'
     },
     // {
@@ -39,14 +39,14 @@ app.get('/shopping', function(req, res) {
     sourceShoppings.forEach(sourceShoppings => {
         const searchAddress = sourceShoppings.address + item
         const nameShopping = sourceShoppings.name
-
         axios.get(searchAddress)
             .then((response) => {
                 const html = response.data
                 const $ = cheerio.load(html)
                 $(sourceShoppings.TAGmainBlockItem, html).each(function() {
-                    const productTitle = $(this).find(sourceShoppings.TAGmainBlockItemName).text()
-                    const price = $(this).find(sourceShoppings.TAGmainBlockItemPrice).text()
+
+                    const productTitle = $(this).find(sourceShoppings.TAGmainBlockItemName).first().text()
+                    const price = $(this).find(sourceShoppings.TAGmainBlockItemPrice).first().text()
                     const url = $(this).find(sourceShoppings.GETmainBlockItemURL).attr('href')
                     if (!productTitle == 0) {
                         itemsList.push({
--- readme.md
+++ readme.md
@@ -8,16 +8,25 @@ The first one is Tesco(ie).
 To use this API you'll need [NPM](https://www.npmjs.com/), after installed you'll need to add the dependencies below:
 
 To check the npm version you can type
+
 ```bash
 npm -v
 ```
+
 You'll need the dependence: [Axios](https://www.npmjs.com/package/axios), [Cheerio](https://www.npmjs.com/package/cheerio), [Express](https://www.npmjs.com/package/express), and [Nodemon](https://www.npmjs.com/package/nodemon)
 This last one is more for development since the objective is to load the API for every change to the code, if you need to install the dependencies you can just type the command below for each dependence.
+
 ```bash
 npm install axios
 ```
 
 ## Using
+
+First of all you can start your npm service runing the following command:
+
+```bash
+npm start
+```
 
 The API-shopping will return a json file with the price of items from TESCO based on the request below:
 
@@ -31,11 +40,12 @@ To check a live example of the API working you can visit this address [API-shopp
 ) and here is a test for the item bananas [API-shopping working and searching for BANANAS](https://api-shopping.herokuapp.com/shopping?item=bananas
 )
 
-
 ## Contributing
+
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
